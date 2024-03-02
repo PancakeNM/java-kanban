8c4cdf5fbe7f100354ae6f361598e5d7782ff621
Pancake <nik.moskovenko@yandex.ru>
2024-03-02 15:52:46 +0300

    add InMemoryHistoryManager & implement add, getHistory, sizeChecker methods

---
+++ src/ru/manager/InMemoryHistoryManager.java
@@ -0,0 +1,27 @@
+package ru.manager;
+
+import ru.manager.interfaces.HistoryManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    List<Task> history = new ArrayList<>();
+
+    @Override
+    public void add (Task task) {
+        sizeChecker();
+        history.add(task);
+    }
+
+    @Override
+    public List<Task> getHistory(){
+        return new ArrayList<>(history);
+    }
+
+    public void sizeChecker() {
+        if (history.size() == 10){
+            history.remove(0);
+        }
+    }
+}
