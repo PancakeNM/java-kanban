02658cb25d1a006e92f779065408ed20b49cafa7
Pancake <nik.moskovenko@yandex.ru>
2024-03-02 15:47:51 +0300

    add getDefaultTaskManager, getDefaultHistoryManager methods

--- src/ru/Managers.java
+++ src/ru/Managers.java
@@ -1,5 +1,17 @@
 package ru;
 
+import ru.manager.InMemoryHistoryManager;
+import ru.manager.InMemoryTaskManager;
+import ru.manager.interfaces.HistoryManager;
+import ru.manager.interfaces.TaskManager;
+
 public class Managers {
 
+    public static TaskManager getDefaultTaskManager() {
+        return new InMemoryTaskManager(getDefaultHistoryManager());
+    }
+
+    public static HistoryManager getDefaultHistoryManager(){
+        return new InMemoryHistoryManager();
+    }
 }
