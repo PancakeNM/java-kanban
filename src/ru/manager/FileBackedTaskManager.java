6bf76550da7d21c36db9f1f50d259eeceb66fca0
Pancake <nik.moskovenko@yandex.ru>
2024-04-16 13:32:18 +0300

    override multiple methods & add save method

--- src/ru/manager/FileBackedTaskManager.java
+++ src/ru/manager/FileBackedTaskManager.java
@@ -10,4 +10,76 @@ public class FileBackedTaskManager extends InMemoryTaskManager{
     public void save() {
 
     }
+
+    @Override
+    public void addNewTask(Task task) { //метод добавления новой задачи.
+        super.addNewTask(task);
+        save();
+    }
+
+    @Override
+    public void addNewSubTask(SubTask subTask) { //метод добавления новой подзадачи.
+        super.addNewSubTask(subTask);
+        save();
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        super.updateTask(task);
+        save();
+    }
+
+    @Override
+    public void updateSubTask(SubTask subTask) {
+        super.updateSubTask(subTask);
+        save();
+    }
+
+    @Override
+    public void updateEpic(Epic epic) {
+        super.updateEpic(epic);
+        save();
+    }
+
+    @Override
+    public void addNewEpic(Epic epic) { //метод добавления нового эпика.
+        super.addNewEpic(epic);
+        save();
+    }
+
+    @Override
+    public void removeAllTasks() { //удаление всех задач
+        super.removeAllTasks();
+        save();
+    }
+
+    @Override
+    public void removeAllEpics() { //удаление всех эпиков
+        super.removeAllEpics();
+        save();
+    }
+
+    @Override
+    public void removeAllSubTasks() { //удаление всех подзадач
+        super.removeAllSubTasks();
+        save();
+    }
+
+    @Override
+    public void removeTaskById(int id) { //удаление задачи по id
+        super.removeTaskById(id);
+        save();
+    }
+
+    @Override
+    public void removeEpicById(int id) { //удаление эпика по id
+        super.removeEpicById(id);
+        save();
+    }
+
+    @Override
+    public void removeSubTaskById(int id) { //удаление подзадачи по id
+        super.removeSubTaskById(id);
+        save();
+    }
 }
