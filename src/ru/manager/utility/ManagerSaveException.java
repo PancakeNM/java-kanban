e372e2a6d721375295e8896029ad9ae26975ceb1
Pancake <nik.moskovenko@yandex.ru>
2024-05-08 00:45:42 +0300

    add custom IO exception;

---
+++ src/ru/manager/utility/ManagerSaveException.java
@@ -0,0 +1,9 @@
+package ru.manager.utility;
+
+import java.io.IOException;
+
+public class ManagerSaveException extends IOException {
+    public ManagerSaveException(String message) {
+        super(message);
+    }
+}
