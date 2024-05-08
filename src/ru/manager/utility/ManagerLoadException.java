1d5bb53319ec9ec77f5b75dbc888f95fa0d325f0
Pancake <nik.moskovenko@yandex.ru>
2024-05-08 19:36:01 +0300

    fix style 2

--- src/ru/manager/utility/ManagerLoadException.java
+++ src/ru/manager/utility/ManagerLoadException.java
@@ -1,6 +1,6 @@
 package ru.manager.utility;
 
-public class ManagerLoadException extends Error{
+public class ManagerLoadException extends Error {
 
     public ManagerLoadException(String message) {
         super(message);
