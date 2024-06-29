6185678ad3f5f09d26637f409aee06311b4be674
Pancake <nik.moskovenko@yandex.ru>
2024-06-29 17:31:11 +0300

    Добавь класс ErrorHandler для обработки ошибок

--- src/ru/manager/api/HttpTaskServer.java
+++ src/ru/manager/api/HttpTaskServer.java
@@ -1,8 +1,12 @@
 package ru.manager.api;
 
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import ru.Managers;
+import ru.manager.api.handler.ErrorHandler;
+import ru.manager.interfaces.TaskManager;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -10,6 +14,30 @@ import java.net.InetSocketAddress;
 public class HttpTaskServer {
     public static HttpServer httpServer;
     public static final int PORT = 8080;
+    public static Gson gson;
+
+    public static TaskManager manager;
+
+    public HttpTaskServer() {
+        gson = new Gson();
+        manager = Managers.getDefaultTaskManager();
+    }
+
+    public static HttpServer getHttpServer() {
+        return httpServer;
+    }
+
+    public static Gson getGson() {
+        return gson;
+    }
+
+    public static TaskManager getManager() {
+        return manager;
+    }
+
+    public void stop() {
+        httpServer.stop(1);
+    }
 
     public static void start() throws IOException {
         httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
@@ -33,7 +61,13 @@ public class HttpTaskServer {
     static class TaskHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange httpExchange) throws IOException {
+            try (httpExchange) {
+                try {
 
+                } catch (Exception e) {
+
+                }
+            }
         }
     }
 
