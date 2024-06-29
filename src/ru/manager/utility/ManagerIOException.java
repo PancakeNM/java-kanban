23eb389ef6a625f8ece6baad5703ca1980d1c29b
Pancake <nik.moskovenko@yandex.ru>
2024-06-29 16:10:00 +0300

    Объедени повторяющие друг друга исключения в одно

---
+++ src/ru/manager/utility/ManagerIOException.java
@@ -0,0 +1,8 @@
+package ru.manager.utility;
+
+public class ManagerIOException extends RuntimeException {
+
+    public ManagerIOException(String message) {
+        super(message);
+    }
+}
--- src/ru/manager/utility/ManagerSaveException.java
+++
@@ -1,7 +0,0 @@
-package ru.manager.utility;
-
-public class ManagerSaveException extends RuntimeException {
-    public ManagerSaveException(String message) {
-        super(message);
-    }
-}
