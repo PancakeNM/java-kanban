b73084165cec71fc8ace838b2c4a9af5b593ec18
Pancake <nik.moskovenko@yandex.ru>
2024-06-30 17:16:04 +0300

    Добавлены токены для конвертации мап различных задач

---
+++ src/ru/manager/api/utility/handlers/utility/EpicMapTypeToken.java
@@ -0,0 +1,10 @@
+package ru.manager.api.utility.handlers.utility;
+
+import com.google.gson.reflect.TypeToken;
+import ru.manager.Epic;
+
+import java.util.Map;
+
+public class EpicMapTypeToken extends TypeToken<Map<Integer, Epic>> {
+
+}
---
+++ src/ru/manager/api/utility/handlers/utility/SubtaskMapTypeToken.java
@@ -0,0 +1,10 @@
+package ru.manager.api.utility.handlers.utility;
+
+import com.google.gson.reflect.TypeToken;
+import ru.manager.SubTask;
+
+import java.util.Map;
+
+public class SubtaskMapTypeToken extends TypeToken<Map<Integer, SubTask>> {
+
+}
